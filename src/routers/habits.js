7a730bcd99fe9c14e6536f1421ee10c0d7397ea2
“liambrockpy” <liambrock72@gmail.com>
2022-12-13 10:01:03 +0000

    Add auth middleware to habit routes

--- src/routers/habits.js
+++ src/routers/habits.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const habitsRouter = express.Router();
-const habitsController = require('../controllers/habits')
-const authenticator = require('../middleware/auth')
+const express = require("express")
+const habitsRouter = express.Router()
+const habitsController = require("../controllers/habits")
+const authenticator = require("../middleware/auth")
 
-habitsRouter.get('/', habitsController.index)
-habitsRouter.get('/:id', habitsController.show)
-habitsRouter.get('/new', habitsController.neww)
-habitsRouter.get('/:id/edit', habitsController.edit)
-habitsRouter.post('/', habitsController.create)
-habitsRouter.put('/:id', habitsController.update)
-habitsRouter.delete('/:id', habitsController.destroy)
+habitsRouter.get("/", authenticator, habitsController.index)
+habitsRouter.get("/:id", authenticator, habitsController.show)
+habitsRouter.get("/new", authenticator, habitsController.neww)
+habitsRouter.get("/:id/edit", authenticator, habitsController.edit)
+habitsRouter.post("/", authenticator, habitsController.create)
+habitsRouter.put("/:id", authenticator, habitsController.update)
+habitsRouter.delete("/:id", authenticator, habitsController.destroy)
 
 module.exports = habitsRouter
-
