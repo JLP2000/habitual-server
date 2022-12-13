03f31286fe28e4c26b3fb97541e41a2f71a4025d
“liambrockpy” <liambrock72@gmail.com>
2022-12-13 09:47:43 +0000

    Update habits methods for finding by user

--- src/controllers/habits.js
+++ src/controllers/habits.js
@@ -1,59 +1,63 @@
-const Habit = require('../models/Habit')
+const Habit = require("../models/Habit")
+const User = require("../models/User")
+const Session = require("../models/Session")
 
-async function index(req, res){
-    try{
-        console.log("hi")
-        const habits = await Habit.all;
-        // console.log(habits);
-        res.status(200).json(habits);
-    } catch(err){
-        res.status(500).json(err);
-    }
+async function index(req, res) {
+	try {
+		const userToken = req.headers["authorization"]
+		const { user_id } = Session.findBySessionToken(userToken)
+		const habits = await Habit.all(user_id)
+		// console.log(habits);
+		res.status(200).json(habits)
+	} catch (err) {
+		res.status(500).json(err)
+	}
 }
 
-async function show(req, res){
-    try{
-        const habit = await Habit.findById(req.params.id);
-        res.status(200).json(habit)
-    } catch(err){
-        res.status(404).json(err);
-    }
+async function show(req, res) {
+	try {
+		const habit = await Habit.findById(req.params.id)
+		res.status(200).json(habit)
+	} catch (err) {
+		res.status(404).json(err)
+	}
 }
 
-async function create(req, res){
-    try{
-        const newHabit = await Habit.create(req.body)
-        res.status(201).json(newHabit)
-    } catch(err){
-        res.status(422).json(err);
-    }
+async function create(req, res) {
+	try {
+		const userToken = req.headers["authorization"]
+		const { user_id } = Session.findBySessionToken(userToken)
+		const newHabit = await Habit.create({ ...req.body, user_id })
+		res.status(201).json(newHabit)
+	} catch (err) {
+		res.status(422).json(err)
+	}
 }
 
-async function destroy(req, res){
-    try{
-        const habit = await Habit.findById(req.params.id)
-        const res = habit.destroy();
-        res.status(204).end();
-    } catch(err){
-        res.status(404).json(err);
-    }
+async function destroy(req, res) {
+	try {
+		const habit = await Habit.findById(req.params.id)
+		const res = habit.destroy()
+		res.status(204).end()
+	} catch (err) {
+		res.status(404).json(err)
+	}
 }
 
-async function update(req, res){
-    try{
-        const habit = await Habit.update(req.body)
-    } catch(err){
-        res.status(417).json(err);
-    }
+async function update(req, res) {
+	try {
+		const habit = await Habit.update(req.body)
+	} catch (err) {
+		res.status(417).json(err)
+	}
 }
 
-async function edit(req, res){
-    res.status(501)
+async function edit(req, res) {
+	res.status(501)
 }
 
-async function neww(req, res){
-    res.status(501)
+async function neww(req, res) {
+	res.status(501)
 }
 
-
-module.exports = {index, show, create, destroy, update, edit, neww}
+module.exports = { index, show, create, destroy, update, edit, neww }
--- src/models/Habit.js
+++ src/models/Habit.js
@@ -1,112 +1,144 @@
-const db = require('../db/init');
-const dayjs = require('dayjs')
-const HabitDate = require('./HabitDate')
+const db = require("../db/init")
+const dayjs = require("dayjs")
+const HabitDate = require("./HabitDate")
 
 class Habit {
-    constructor(data) {
-        this.habit_id = data.habit_id;
-        this.name = data.name;
-        this.start_date = data.start_date;
-        this.interval_in_days = data.interval_in_days;
-        this.interval_in_months = data.interval_in_months;
-        this.end_date = data.end_date;
-        this.note = data.note;
-        this.colour = data.colour;
-    }
+	constructor(data) {
+		this.habit_id = data.habit_id
+		this.name = data.name
+		this.start_date = data.start_date
+		this.interval_in_days = data.interval_in_days
+		this.interval_in_months = data.interval_in_months
+		this.end_date = data.end_date
+		this.note = data.note
+		this.colour = data.colour
+	}
 
-    static get all() {
-        return new Promise (async (resolve, reject) => {
-            try{
-                let response = await db.query("SELECT * FROM habits;");
-                console.log(response)
-                let habits = response.rows.map(h => new Habit(h))
-                resolve(habits);
-            } catch(err){
-                reject("Habit not found")
-            }
-        })
-    }
+	static async all(userId) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let response = await db.query(
+					"SELECT * FROM habits WHERE $1 = user_id;",
+					[userId]
+				)
+				console.log(response)
+				let habits = response.rows.map((h) => new Habit(h))
+				resolve(habits)
+			} catch (err) {
+				reject("Habit not found")
+			}
+		})
+	}
 
-    static async findById(id) {
-        return new Promise (async (resolve, reject) => {
-           try { 
-                let habitData = await db.query("SELECT * FROM habits WHERE habit_id = $1;", [id]);
-                // console.log(habitData);
-                let habit = new Habit(habitData.rows[0]);
-                resolve(habit)
-            } catch(err) {
-                reject("Habit not found")
-                console.log(err)
-            }
-        })  
-    }
+	static async findById(id) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let habitData = await db.query(
+					"SELECT * FROM habits WHERE habit_id = $1;",
+					[id]
+				)
+				// console.log(habitData);
+				let habit = new Habit(habitData.rows[0])
+				resolve(habit)
+			} catch (err) {
+				reject("Habit not found")
+				console.log(err)
+			}
+		})
+	}
 
-    static async create(habitData) {
-        return new Promise (async (resolve, reject) => {
-            try {
-                // console.log(habitData)
-                const { name, start_date, interval_in_days, interval_in_months, end_date, note, colour, user_id} = habitData;
-                const result = await db.query("INSERT INTO habits (name, start_date, interval_in_days, interval_in_months, end_date, note, colour, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *", [name, start_date, interval_in_days, interval_in_months, end_date, note, colour, user_id]);
-                // console.log(result);
-                let newHabit = new Habit(result.rows[0]);
-                newHabit.createDates();
-                resolve(newHabit);
-            } catch (err) {
-                reject("Could not create habit");
-                console.log(err);
-            }
-        })
-    }
+	static async create(habitData) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				// console.log(habitData)
+				const {
+					name,
+					start_date,
+					interval_in_days,
+					interval_in_months,
+					end_date,
+					note,
+					colour,
+					user_id,
+				} = habitData
+				const result = await db.query(
+					"INSERT INTO habits (name, start_date, interval_in_days, interval_in_months, end_date, note, colour, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+					[
+						name,
+						start_date,
+						interval_in_days,
+						interval_in_months,
+						end_date,
+						note,
+						colour,
+						user_id,
+					]
+				)
+				// console.log(result);
+				let newHabit = new Habit(result.rows[0])
+				newHabit.createDates()
+				resolve(newHabit)
+			} catch (err) {
+				reject("Could not create habit")
+				console.log(err)
+			}
+		})
+	}
 
-    async destroy() {
-        return new Promise (async (resolve, reject) => {
-            try {
-                const result = await db.query("DELETE FROM habits WHERE habit_id = $1;", [this.habit_id]);
-                resolve("Habit was destroyed");
-            } catch (err) {
-                reject("Could not destroy habit")
-                console.log(err)
-            }
-        })
-    }
+	async destroy() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const result = await db.query(
+					"DELETE FROM habits WHERE habit_id = $1;",
+					[this.habit_id]
+				)
+				resolve("Habit was destroyed")
+			} catch (err) {
+				reject("Could not destroy habit")
+				console.log(err)
+			}
+		})
+	}
 
-    static async update(habitData) {
-        return new Promise (async (resolve, reject) => {
-            try {
-                const {habit_id, name, note, colour} = habitData;
-                const result = await db.query("UPDATE habits SET name = $1, note = $2, colour = $3 WHERE habit_id = $4 RETURNING *;", [name, note, colour, habit_id]);
-                resolve(result.rows[0])
-            } catch (err) {
-                reject("Could not update habit");
-                console.log(err);
-            }
-        })
-    }
+	static async update(habitData) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const { habit_id, name, note, colour } = habitData
+				const result = await db.query(
+					"UPDATE habits SET name = $1, note = $2, colour = $3 WHERE habit_id = $4 RETURNING *;",
+					[name, note, colour, habit_id]
+				)
+				resolve(result.rows[0])
+			} catch (err) {
+				reject("Could not update habit")
+				console.log(err)
+			}
+		})
+	}
 
-    async createDates() {
-        return new Promise (async (resolve, reject) => {
-            try {
-                let date = dayjs(this.start_date)
-                HabitDate.create(this.habit_id, date);
-                if (this.interval_in_months == 0) {
-                    while(date.diff(dayjs(this.end_date), 'day') != 0) {
-                        date = dayjs(date).add(this.interval_in_days, 'day');
-                        let result = HabitDate.create(this.habit_id, date);
-                        
-                    }
-                } else if (this.interval_in_days == 0) {
-                    while (date.diff(dayjs(this.end_date), 'month') != 0) {
-                        date = dayjs(date).add(this.interval_in_months, 'month');
-                        let result1 = HabitDate.create(this.habit_id, date);
-                    }
-                }
-                resolve("Dates added")
-            } catch (err) {
-                reject("Could not add dates");
-                console.log(err);
-            }
-        })
-    }
+	async createDates() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let date = dayjs(this.start_date)
+				HabitDate.create(this.habit_id, date)
+				if (this.interval_in_months == 0) {
+					while (date.diff(dayjs(this.end_date), "day") != 0) {
+						date = dayjs(date).add(this.interval_in_days, "day")
+						let result = HabitDate.create(this.habit_id, date)
+					}
+				} else if (this.interval_in_days == 0) {
+					while (date.diff(dayjs(this.end_date), "month") != 0) {
+						date = dayjs(date).add(this.interval_in_months, "month")
+						let result1 = HabitDate.create(this.habit_id, date)
+					}
+				}
+				resolve("Dates added")
+			} catch (err) {
+				reject("Could not add dates")
+				console.log(err)
+			}
+		})
+	}
 }
 
-module.exports = Habit;
+module.exports = Habit
