2195662345087706956302c20098de41513edcfe
“liambrockpy” <liambrock72@gmail.com>
2022-12-13 10:25:08 +0000

    Fix auth middleware method

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,23 @@
-const Session = require('../models/Session');
+const Session = require("../models/Session")
 
 async function authenticator(req, res, next) {
+	try {
+		// Extract the token from the request
+		const userToken = req.headers["authorization"]
+		console.log("auth middleware: ", userToken)
+		if (userToken == "null") {
+			console.log(userToken)
+			throw new Error("Missing token")
+		} else {
+			// Validate it
+			const validToken = await Session.findBySessionToken(userToken)
+			next()
+		}
+	} catch (err) {
+		console.log(err)
 
-    try {
-
-        // Extract the token from the request
-        const userToken = req.headers["authorization"]; 
-        if (userToken == "null") {
-            console.log(userToken)
-            throw new Error("Missing token");
-        } else {
-            // Validate it
-            const validToken = await Session.getOneBySessionToken(userToken);
-            next();
-        }
-        
-
-    } catch (err) {
-        console.log(err);
-
-        res.status(401).json({error: "Invalid credentials"})
-    }
+		res.status(401).json({ error: "Invalid credentials" })
+	}
 }
 
-module.exports = authenticator;
+module.exports = authenticator
